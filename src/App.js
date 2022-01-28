847d81f3083d2e1af6460c17a489cc35dddc3116
WangYuKai <kenw860409@gmail.com>
2022-01-28 19:14:15 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+import { COPYRIGHT_YEAR, ARTHUR } from "./constants/Data";
+import HeaderFilter from "./components/HeaderFilter";
+import TodoContainer from "./components/TodoContainer";
+
+const TodoWrapper = styled.div`
+  width: 100%;
+  min-height: 90%;
+  padding: 0 15px;
+  padding-top: 85px;
+  padding-bottom: 35px;
+  background: #e1e1e1;
+`;
+
+const Footer = styled.footer`
+  margin: 0 auto;
+  height: 50px;
+  text-align: center;
+  padding: 15px 0;
+  background: white;
+
+  span {
+    color: brown;
+    font-family: Comic Sans MS;
+    font-style: italic;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <HeaderFilter />
+      <TodoWrapper>
+        <TodoContainer />
+      </TodoWrapper>
+      <Footer>
+        Copyright &copy; {COPYRIGHT_YEAR} by <span>{ARTHUR}</span> All Rights
+        Reserved.
+      </Footer>
+    </>
   );
 }
 
